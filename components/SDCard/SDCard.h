80a885be718d72090180bdc1e2851fd9927f366c
eremiticengineer <dev@eremiticengineer.com>
2026-04-12 15:50:58 +0100

    Refactor to support MMC as well as SPI
    Rename init to initSPI
    Add initMMC

--- components/SDCard/SDCard.h
+++ components/SDCard/SDCard.h
@@ -7,7 +7,8 @@ class SDCard {
 public:
     SDCard();
     ~SDCard();
-    esp_err_t init(const char* mountPoint);
+    esp_err_t initSPI(const char* mountPoint);
+    esp_err_t initMMC(const char* mountPoint);
     esp_err_t writeFile(const char *path, char *data);
     esp_err_t readFile(const char *path, std::string& fileContent);
 
