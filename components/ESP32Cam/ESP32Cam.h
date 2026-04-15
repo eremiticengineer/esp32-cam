09d836a6707658f255e691c145a7f59035ce8dc3
eremiticengineer <dev@eremiticengineer.com>
2026-04-15 17:21:03 +0100

    Refactor for messaging

--- components/ESP32Cam/ESP32Cam.h
+++ components/ESP32Cam/ESP32Cam.h
@@ -1,20 +1,22 @@
 #pragma once
 
+#include "bus.h"
+#include "messages.h"
 #include <string>
 #include "esp_check.h"
 
 class ESP32Cam {
 public:
-
-    struct ImageData {
-        uint8_t* data;
-        size_t len;
-    };
-
     ESP32Cam();
     ~ESP32Cam();
     esp_err_t init_camera();
     ImageData capture_image();
+    void start(SystemBus* bus);
 
 private:
+    SystemBus* _bus = nullptr;
+    TaskHandle_t _taskHandle = nullptr;
+
+    static void task_wrapper(void* arg);
+    void run();
 };
