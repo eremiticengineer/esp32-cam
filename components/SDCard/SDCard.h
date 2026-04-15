49e7b552e84810aa2437f1b7d4a6f7f5faaed549
eremiticengineer <dev@eremiticengineer.com>
2026-04-15 10:51:11 +0100

    Refactor to store mount point
    Add const std::string& get_mount_point() const;
    Add std::string _mountPoint;

--- components/SDCard/SDCard.h
+++ components/SDCard/SDCard.h
@@ -12,6 +12,8 @@ public:
     esp_err_t write_file(const char *path, char *data);
     esp_err_t write_binary_file(const char *path, const uint8_t *data, size_t len);
     esp_err_t read_file(const char *path, std::string& fileContent);
+    const std::string& get_mount_point() const;
 
 private:
+    std::string _mountPoint;
 };
