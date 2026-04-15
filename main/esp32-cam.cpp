2785f960eb688c2dedc4c7e24f51fc55683b3ae7
eremiticengineer <dev@eremiticengineer.com>
2026-04-15 09:38:30 +0100

    Refactor for ESP32Cam class
    Move camera logic to ESP32Cam class
    Add simple UART reading

--- main/esp32-cam.cpp
+++ main/esp32-cam.cpp
@@ -6,90 +6,69 @@
 #include <sys/param.h>
 #include <string.h>
 #include "driver/i2c_master.h"
+#include "driver/uart.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#include "esp_camera.h"
-
 #include "SDCard.h"
+#include "ESP32Cam.h"
 
 static const char *TAG = "ESP32CAM";
 
 #define LOG_ERR(tag, err, msg) \
     ESP_LOGE(tag, "%s: %s (0x%x)", msg, esp_err_to_name(err), (unsigned int)(err))
 
-// Pin definitions for CAMERA_MODEL_AI_THINKER
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
-
-static camera_config_t camera_config;
-
-void init_camera_config() {
-  camera_config.ledc_channel = LEDC_CHANNEL_0;
-  camera_config.ledc_timer = LEDC_TIMER_0;
-  camera_config.pin_d0 = Y2_GPIO_NUM;
-  camera_config.pin_d1 = Y3_GPIO_NUM;
-  camera_config.pin_d2 = Y4_GPIO_NUM;
-  camera_config.pin_d3 = Y5_GPIO_NUM;
-  camera_config.pin_d4 = Y6_GPIO_NUM;
-  camera_config.pin_d5 = Y7_GPIO_NUM;
-  camera_config.pin_d6 = Y8_GPIO_NUM;
-  camera_config.pin_d7 = Y9_GPIO_NUM;
-  camera_config.pin_xclk = XCLK_GPIO_NUM;
-  camera_config.pin_pclk = PCLK_GPIO_NUM;
-  camera_config.pin_vsync = VSYNC_GPIO_NUM;
-  camera_config.pin_href = HREF_GPIO_NUM;
-  camera_config.pin_sccb_sda = SIOD_GPIO_NUM;
-  camera_config.pin_sccb_scl = SIOC_GPIO_NUM;
-  camera_config.pin_pwdn = PWDN_GPIO_NUM;
-  camera_config.pin_reset = RESET_GPIO_NUM;
-  camera_config.xclk_freq_hz = 20000000;
-  camera_config.pixel_format = PIXFORMAT_JPEG; // Choices are YUV422, GRAYSCALE, RGB565, JPEG
-  camera_config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
-  camera_config.jpeg_quality = 10; //10-63 lower number means higher quality
-  camera_config.fb_count = 2;
-  camera_config.fb_location = CAMERA_FB_IN_PSRAM;
-  camera_config.grab_mode = CAMERA_GRAB_LATEST;
-}
-
-static esp_err_t init_camera(void)
+static TaskHandle_t uartTaskHandle;
+static void uart_task(void *arg)
 {
-    esp_err_t err = esp_camera_init(&camera_config);
-    if (err != ESP_OK)
-    {
-        ESP_LOGE(TAG, "Camera Init Failed");
-        return err;
-    }
+    uint8_t data[1024];
 
-    return ESP_OK;
+    const uart_port_t uart_num = UART_NUM_0;
+
+    uart_config_t uart_config;
+    uart_config.baud_rate = 115200;
+    uart_config.data_bits = UART_DATA_8_BITS;
+    uart_config.parity    = UART_PARITY_DISABLE;
+    uart_config.stop_bits  = UART_STOP_BITS_1;
+    uart_config.flow_ctrl  = UART_HW_FLOWCTRL_DISABLE;
+    uart_config.source_clk = UART_SCLK_DEFAULT;
+    uart_config.rx_glitch_filt_thresh = 0;
+
+    esp_err_t err = uart_param_config(uart_num, &uart_config);
+    uart_driver_install(uart_num, 2048, 0, 0, nullptr, 0);
+    if (ESP_OK != err) {
+      LOG_ERR(TAG, err, "cannot config UART");
+      // ESP_ERROR_CHECK(uart_param_config(UART_NUM_1, &uart_config));
+      vTaskDelete(NULL);
+    }
+    ESP_LOGI(TAG, "UART ready");
+
+    while (1) {
+        int len = uart_read_bytes(uart_num, data, 1024 - 1, pdMS_TO_TICKS(100));
+
+        if (len > 0) {
+            data[len] = 0; // null-terminate for string handling
+
+            ESP_LOGI(TAG, "RECEIVED: %s", (char*)data);
+
+            if (strcmp((char *)data, "pic:capture") == 0) {
+                // trigger capture
+                ESP_LOGI(TAG, "received!");
+                //handle_capture();
+            }
+        }
+    }
 }
 
 extern "C" void app_main(void)
 {
-    init_camera_config();
-    esp_err_t camera_err;
-    camera_err = init_camera();
-    if(ESP_OK != camera_err) {
-      while (1) {
-        LOG_ERR(TAG, camera_err, "cannot init camera");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-      }
+    for (int i = 0; i < 5; i++) {
+      ESP_LOGI(TAG, "Time elapsed: %d s", i + 1);
+      vTaskDelay(pdMS_TO_TICKS(1000));
     }
+
+    xTaskCreate(uart_task, "uart_task", 4096, NULL, 5, &uartTaskHandle);
 
     SDCard sdcard;
     esp_err_t ret = sdcard.init_mmc("/sdcard");
@@ -116,6 +95,9 @@ extern "C" void app_main(void)
         }
     }
 
+    ESP32Cam camera;
+    camera.init_camera();
+
     int photoId = 1;
     char filename[32];
     while (1)
@@ -130,23 +112,21 @@ extern "C" void app_main(void)
             ESP_LOGI(TAG, "%s", fileContent.c_str());
         }
 
-        ESP_LOGI(TAG, "Taking picture...");
-        camera_fb_t* pic = esp_camera_fb_get();
-        if (pic != NULL) {
-            ESP_LOGI(TAG, "Picture taken! Its size was: %zu bytes", pic->len);
-            snprintf(filename, sizeof(filename), "/sdcard/image_%d.jpg", photoId++);
-            ret = sdcard.write_binary_file(filename, pic->buf, pic->len);
-            if (ESP_OK == ret) {
-              ESP_LOGI(TAG, "wrote image to sdcard:");
-              ESP_LOGI(TAG, "%s", filename);
-            }
-            else {
-              LOG_ERR(TAG, ret, "cannot write image to sdcard");
-            }
-            esp_camera_fb_return(pic);
+        ESP32Cam::ImageData image = camera.capture_image();
+        if (image.data != nullptr) {
+          ESP_LOGI(TAG, "Picture taken! Its size was: %zu bytes", image.len);
+          snprintf(filename, sizeof(filename), "/sdcard/image_%d.jpg", photoId++);
+          ret = sdcard.write_binary_file(filename, image.data, image.len);
+          if (ESP_OK == ret) {
+            ESP_LOGI(TAG, "wrote image to sdcard:");
+            ESP_LOGI(TAG, "%s", filename);
+          }
+          else {
+            LOG_ERR(TAG, ret, "cannot write image to sdcard");
+          }
         }
         else {
-          ESP_LOGE(TAG, "failed to take picture");
+          ESP_LOGE(TAG, "could not capture image");
         }
 
         vTaskDelay(5000 / portTICK_PERIOD_MS);
