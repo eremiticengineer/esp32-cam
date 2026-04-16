6c9862d76e99d9c19b37c252819d7b2be0fd26b8
eremiticengineer <dev@eremiticengineer.com>
2026-04-16 11:21:10 +0100

    Refactor for allowing init and two tasks
    Add esp_err_t init();
    Add TaskHandle_t _taskHandleEventListener = nullptr;
    Add static void event_task_wrapper(void *arg);

--- components/UartAPI/UartAPI.h
+++ components/UartAPI/UartAPI.h
@@ -4,13 +4,17 @@ class UartAPI {
 
 public:
   UartAPI();
-  void start(SystemBus* bus);
+  esp_err_t init();
+  void start(SystemBus *bus);
   void run();
+  void event_listener();
 
 private:
   SystemBus* _bus = nullptr;
   TaskHandle_t _taskHandle = nullptr;
+  TaskHandle_t _taskHandleEventListener = nullptr;
   int _photo_id;
 
   static void task_wrapper(void* arg);
+  static void event_task_wrapper(void *arg);
 };
