fdbe1a6abe423132856f581c2536b494db5f21a8
eremiticengineer <dev@eremiticengineer.com>
2026-04-16 11:18:56 +0100

    Add init method

--- components/Common/include/bus.h
+++ components/Common/include/bus.h
@@ -7,7 +7,13 @@
 struct SystemBus {
     QueueHandle_t commandQueue;
     QueueHandle_t eventQueue;
-
     QueueHandle_t cameraQueue;
     QueueHandle_t sdQueue;
+
+    void init() {
+        commandQueue = xQueueCreate(10, sizeof(Command));
+        eventQueue   = xQueueCreate(10, sizeof(Event));
+        cameraQueue  = xQueueCreate(5, sizeof(Command));
+        sdQueue      = xQueueCreate(5, sizeof(Command));
+    }
 };
