3d37265623a340ea45af330405557c96242a49fa
eremiticengineer <dev@eremiticengineer.com>
2026-04-18 16:54:29 +0100

    Add new methods for new command/response/data framing
    void process_uart_bytes(const uint8_t* input, size_t len);
    void on_command(const std::string& cmd);
    void on_response(const std::string& resp);
    void on_data(const uint8_t* data, size_t len);

--- components/UartAPI/UartAPI.h
+++ components/UartAPI/UartAPI.h
@@ -1,5 +1,7 @@
 #include "bus.h"
 
+#include <string>
+
 class UartAPI {
 
 public:
@@ -17,4 +19,8 @@ private:
 
   static void task_wrapper(void* arg);
   static void event_task_wrapper(void *arg);
+  void process_uart_bytes(const uint8_t* input, size_t len);
+  void on_command(const std::string& cmd);
+  void on_response(const std::string& resp);
+  void on_data(const uint8_t* data, size_t len);
 };
