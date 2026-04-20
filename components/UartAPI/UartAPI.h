032a17b300fc21a7c38f8c34dc16e96a27a19b2f
eremiticengineer <dev@eremiticengineer.com>
2026-04-20 15:05:35 +0100

    Refactor for uart configuration and messaging
    init now takes uart number and tx and rx pin numbers
    Rename event listener and task wrapper variables for clarity

--- components/UartAPI/UartAPI.h
+++ components/UartAPI/UartAPI.h
@@ -1,24 +1,33 @@
-#include "bus.h"
+#pragma once
 
 #include <string>
+
+#include "esp_log.h"
+#include "esp_err.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "bus.h"
 
 class UartAPI {
 
 public:
   UartAPI();
-  esp_err_t init();
-  void start(SystemBus *bus);
+  esp_err_t init(int uart_num, int txPin, int rxPin);
+  void start(SystemBus* bus);
   void run();
   void event_listener();
+  void request(const std::string& request);
 
 private:
-  SystemBus* _bus = nullptr;
-  TaskHandle_t _taskHandle = nullptr;
-  TaskHandle_t _taskHandleEventListener = nullptr;
+  TaskHandle_t _eventListenerTaskHandle = nullptr;
+  TaskHandle_t _runTaskHandle = nullptr;
+  SystemBus* _bus;
   int _photo_id;
 
-  static void task_wrapper(void* arg);
-  static void event_task_wrapper(void *arg);
+  static void event_listener_task_wrapper(void* arg);
+  static void run_task_wrapper(void* arg);
   void process_uart_bytes(const uint8_t* input, size_t len);
   void on_command(const std::string& cmd);
   void on_response(const std::string& resp);
