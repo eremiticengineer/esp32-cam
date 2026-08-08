239e33ea9ae4d547c477581b5203c398c6287a3c
eremiticengineer <dev@eremiticengineer.com>
2026-08-08 11:29:35 +0100

    init now takes UARTConfig

--- components/UartAPI/UartAPI.cpp
+++ components/UartAPI/UartAPI.cpp
@@ -17,8 +17,8 @@ UartAPI::UartAPI() {
     _photo_id = 1;
 }
 
-esp_err_t UartAPI::init(int uart_num, int txPin, int rxPin) {
-    _uart_num = static_cast<uart_port_t>(uart_num);
+esp_err_t UartAPI::init(const UARTConfig& config) {
+    _uart_num = static_cast<uart_port_t>(config.uart_num);
 
     esp_err_t status;
 
@@ -35,11 +35,11 @@ esp_err_t UartAPI::init(int uart_num, int txPin, int rxPin) {
         err = uart_param_config(_uart_num, &uart_config);
         if (ESP_OK == err) {
             err = uart_set_pin(_uart_num,
-                            txPin,
-                            rxPin,
+                            config.tx,
+                            config.rx,
                             UART_PIN_NO_CHANGE,
                             UART_PIN_NO_CHANGE);
-            gpio_set_pull_mode(static_cast<gpio_num_t>(rxPin), GPIO_PULLUP_ONLY);
+            gpio_set_pull_mode(static_cast<gpio_num_t>(config.rx), GPIO_PULLUP_ONLY);
             status = ESP_OK;
             ESP_LOGI(TAG, "UART ready");
             if (ESP_OK != err) {
